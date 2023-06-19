e43f461af7751d9d04c7b57ef05c289a6c236b3c
Lucas Hartley <LukeIsPhoenix@gmail.com>
2023-06-19 01:13:36 -0700

    inital commit

---
+++ chadrc.lua
@@ -0,0 +1,8 @@
+--options
+vim.opt.shell = 'C:/Program Files/Git/bin/bash.exe'
+vim.opt.shellcmdflag = '-c'
+vim.opt.scrolloff = 5
+---@type ChadrcConfig 
+ local M = {}
+ M.ui = {theme = 'doomchad'}
+ return M
